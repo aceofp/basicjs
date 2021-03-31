a791818e2e1d6ac7bad22d81861fe81a1bf79474
kermit <aceofp@gmail.com>
2021-03-31 19:19:15 +0200

    initial commit

---
+++ comment.js
@@ -0,0 +1,8 @@
+// This is a comment   
+// console.log('pyza chciał 3 dychy w pomp')
+
+/*
+console.log('jednak nie pożyczył hajsu')
+console.log('jebać biedę')
+console.log('szynka wyszła perfekcyjnie')
+*/
