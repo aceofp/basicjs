617c20dc9d20fcbe0ea9eb42b6061f8f5390bf67
kermit <aceofp@gmail.com>
2021-04-22 19:08:52 +0200

    Different types of strings

---
+++ strings.js
@@ -0,0 +1,14 @@
+const Jonas = "Jonas is a 46 year \"old\" teacher";
+console.log(Jonas);
+
+var Janusz = 'Janusz is a \'minor\' thief'
+console.log(Janusz);
+
+var PureEvil = 'I\'m PureEvil';
+console.log(PureEvil);
+
+var SoberGuy = "I'm sober for almost two years";
+console.log(SoberGuy);
+
+var Buka = `He's the real "deal" about alcohol`;
+console.log(Buka);
