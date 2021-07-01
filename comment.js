209cee6c1a082ab0587c01acd908f271a2177572
Krzysztof Bednarczyk <Krzysztof_Bednarczyk@epam.com>
2021-07-01 12:43:43 +0200

    Another change to comment file

--- comment.js
+++ comment.js
@@ -24,4 +24,6 @@ console.log('multifloric')
 /*
 I am practicing working with AWS. Today I'm uploading files to check how S3 bucket versioning works. Let's hope I learn a lot today ;)
 Now let's check how it works if I create another version of this file. Now it should have 3 versions on my S3 bucket
+This doesn't work like git at all :P
+It registers a new version every time You upload the file to the S3 bucket. The files don't have to differ.
 */
